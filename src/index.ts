a76da34d476c863c380b4c6fa2c054a60df4a006
meluiz <me.luizfelipe@gmail.com>
2023-09-17 15:07:39 -0300

    chore(index.ts): add import statement for Logger class and export it feat(index.ts): add export statements for lib and utils modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello from unoout')
+import { Logger } from './logger'
+
+export * from './logger'
+
+const logger = new Logger({
+  output: 'log',
+  datatime: false,
+})
+
+export default logger
+
+export * from './lib'
+export * from './utils'
