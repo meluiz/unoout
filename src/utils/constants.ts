25f55a38e00c8cc0dd8fc3cbc9ddc3adc1e60f58
meluiz <me.luizfelipe@gmail.com>
2023-09-17 14:49:34 -0300

    feat(constants.ts): add constants for icons and boxes to improve code readability and maintainability
    The `constants.ts` file was added to the `src/utils` directory. It contains constants for icons and boxes used in the application.
    
    The `icons` constant is exported and its value is determined based on the platform. If the platform is Windows without the `WT_SESSION` environment variable, it uses specific icons for that condition. Otherwise, it uses icons for non-Windows platforms or Windows with `WT_SESSION`.
    
    The `boxes` constant represents different types of boxes and their respective characters for each side. It includes `single`, `double`, `round`, `bold`, and `classic` box types.
    
    These constants were added to improve code readability and maintainability by providing a centralized place for managing icons and boxes used throughout the application.

---
+++ src/utils/constants.ts
@@ -0,0 +1,78 @@
+const isWinWithoutWTSession = process.platform === 'win32' && !process.env.WT_SESSION
+
+// Icons for the specific Windows condition (without WT_SESSION)
+const winWithoutWTSessionIcons = {
+  tick: '√',
+  cross: '×',
+  bullet: '*',
+  nodejs: '♦',
+  info: 'i',
+  warning: '‼',
+} as const
+
+// Icons for non-Windows platforms or Windows with WT_SESSION
+const otherPlatformIcons = {
+  tick: '✔',
+  cross: '✖',
+  circle: '●',
+  nodejs: '⬢',
+  info: 'ℹ',
+  warning: '⚠',
+} as const
+
+// Export the appropriate icons based on the platform
+export const icons = isWinWithoutWTSession ? winWithoutWTSessionIcons : otherPlatformIcons
+
+// Represents different types of boxes.
+export const boxes = {
+  single: {
+    topLeft: '┌',
+    top: '─',
+    topRight: '┐',
+    right: '│',
+    bottomRight: '┘',
+    bottom: '─',
+    bottomLeft: '└',
+    left: '│',
+  },
+  double: {
+    topLeft: '╔',
+    top: '═',
+    topRight: '╗',
+    right: '║',
+    bottomRight: '╝',
+    bottom: '═',
+    bottomLeft: '╚',
+    left: '║',
+  },
+  round: {
+    topLeft: '',
+    top: '',
+    topRight: '',
+    right: '',
+    bottomRight: '',
+    bottom: '',
+    bottomLeft: '',
+    left: '',
+  },
+  bold: {
+    topLeft: '╭',
+    top: '─',
+    topRight: '╮',
+    right: '│',
+    bottomRight: '╯',
+    bottom: '─',
+    bottomLeft: '╰',
+    left: '│',
+  },
+  classic: {
+    topLeft: '+',
+    top: '-',
+    topRight: '+',
+    right: '|',
+    bottomRight: '+',
+    bottom: '-',
+    bottomLeft: '+',
+    left: '|',
+  },
+} as const
