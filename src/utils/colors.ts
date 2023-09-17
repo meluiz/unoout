82a6767222265fb9226864136e8ff6f55356271a
meluiz <me.luizfelipe@gmail.com>
2023-09-17 14:49:00 -0300

    feat(colors.ts): add colors module to enable formatting in the application
    The colors module is added to the project to enable formatting. It creates and returns a colors object that can be used for formatting text in the application. The `createColors` function from the `picocolors` library is used to create the colors object. The `enableFormatting` parameter is set to `true` to enable formatting.

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+import { createColors } from 'picocolors'
+
+/**
+ * Creates and returns a colors object.
+ *
+ * @param {boolean} enableFormatting - Whether to enable formatting.
+ * @returns {object} - The colors object.
+ */
+export const colors = createColors(true)
