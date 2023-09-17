12d3d7b509cc4048a5cc15fc07489ccf8fa39f49
meluiz <me.luizfelipe@gmail.com>
2023-09-17 14:46:09 -0300

    feat(log-entry.ts): add logEntry function to log messages with configurable options
    The logEntry function is added to the log-entry.ts file. This function allows logging messages with various options. The options include the message itself, the log level, the output destination (either 'log' or 'stdout'), and whether to include the current datetime in the log message.
    
    The function destructures the options object and assigns default values to the properties if they are not provided. It then determines the appropriate log function based on the output option.
    
    The function creates an array to hold the log message components and adds the current datetime and log level if provided. It then joins the log message components into a single string and calls the appropriate log function with the formatted message.
    
    This function provides flexibility in logging messages with different options, improving the logging capabilities of the application.

---
+++ src/lib/log-entry.ts
@@ -0,0 +1,44 @@
+import { log } from 'console'
+import { stdout } from './stdout'
+
+// Define the shape of a log entry
+interface LogEntry {
+  message: string
+  level?: string
+  output: 'log' | 'stdout'
+  datatime?: boolean
+}
+
+/**
+ * Log an entry with the given options.
+ * @param options - The options for the log entry.
+ */
+export function logEntry(options: LogEntry) {
+  // Destructure the options object
+  const { message, level, output = 'stdout', datatime = false } = options
+
+  // Determine the log function based on the output option
+  const logFunction = output === 'log' ? log : stdout
+
+  // Create an array to hold the log message components
+  const logMessage = []
+
+  // Add the current datetime to the log message if datatime is true
+  if (datatime) {
+    logMessage.push(new Date().toISOString())
+  }
+
+  // Add the log level to the log message if provided
+  if (level) {
+    logMessage.push(level)
+  }
+
+  // Add the log message itself
+  logMessage.push(message)
+
+  // Join the log message components into a single string
+  const formated = logMessage.join(' ')
+
+  // Call the appropriate log function with the formatted message
+  logFunction(formated)
+}
